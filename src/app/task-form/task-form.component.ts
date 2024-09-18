cfccb51d084a862cb89c27558511e12e60bcabed
J. Adrian Ruiz C. <ed.alanpoeta@gmail.com>
2024-09-18 12:40:02 -0600

    feat: update TaskForm with CRUD operation for task management

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.css'
+  styleUrls: ['./task-form.component.css'],
 })
 export class TaskFormComponent {
+  taskForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private taskService: TaskService) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      this.taskService.createTask(this.taskForm.value).subscribe(() => {
+        this.taskForm.reset();
+      });
+    }
+  }
 }
