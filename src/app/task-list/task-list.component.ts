21c8d808a1ca515f009681cc3569b9d64b9bfe4b
J. Adrian Ruiz C. <ed.alanpoeta@gmail.com>
2024-09-18 12:37:50 -0600

    feat: update TaskService with CRUD operation for task management

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TaskService, Task } from '../task.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatCheckboxModule, MatButtonModule],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrls: ['./task-list.component.css'],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  toggleCompletion(task: Task): void {
+    task.completed = !task.completed;
+    if (task.id) {
+      this.taskService.updateTask(task.id, task).subscribe(() => {
+        this.loadTasks();
+      });
+    }
+  }
+
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id).subscribe(() => {
+      this.loadTasks();
+    });
+  }
 }
