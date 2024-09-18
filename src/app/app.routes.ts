1cb43a663708dc0a8794040a934050d2411d32d6
J. Adrian Ruiz C. <ed.alanpoeta@gmail.com>
2024-09-18 12:42:37 -0600

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'add-task', component: TaskFormComponent },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+];
